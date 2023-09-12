ea2ea3a9359659559f21ed76663a686308d2ea4b
Jarrad Law <jarrad.law@gmail.com>
2023-09-12 19:59:11 +0930

    Added most of new week's gifts. Added JSON generation file

---
+++ fileGeneration.js
@@ -0,0 +1,53 @@
+function generateString(character) {
+    let template = `    "xxxGift1": {
+        "name": "",
+        "type": "",
+        "updated": 0
+    },
+    "xxxGift2": {
+        "name": "",
+        "type": "",
+        "updated": 0
+    },
+    "xxxGift3": {
+        "name": "",
+        "type": "",
+        "updated": 0
+    },
+    "xxxGift4": {
+        "name": "",
+        "type": "",
+        "updated": 0
+    },`;
+    return template.replaceAll(/xxx/g, character);
+}
+
+let characters = [
+    "Ashura",
+    "Auni",
+    "Badruu",
+    "Caleri",
+    "Chayne",
+    "Delaila",
+    "Einar",
+    "Elouisa",
+    "Eshe",
+    "Hassian",
+    "Hekla",
+    "Hodari",
+    "Jel",
+    "Jina",
+    "Kenli",
+    "Kenyatta",
+    "Nai'O",
+    "Najuma",
+    "Reth",
+    "Sifuu",
+    "Tamala",
+    "Tish",
+    "Zeki",
+];
+
+for (var i = 0; i < characters.length; i++) {
+    console.log(generateString(characters[i]));
+}
