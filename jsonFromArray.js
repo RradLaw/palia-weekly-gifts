750b2091a5df6d65773db2886fe666b663b1ac9e
Kira23x <astreykl@gmail.com>
2023-12-13 16:05:24 +1030

    Gifts updated

--- jsonFromArray.js
+++ jsonFromArray.js
@@ -4,36 +4,31 @@ let errorString = "";
 
 let data = {
 
-
-    "Ashura": ["Channel Catfish", "Golden Salmon", "Flow-Infused Plank", "Fisherman's Brew"],
-    "Auni": ["Common Bark Cicada", "Cerulean Cicada", "Scintillating Centipede", "Apple Pie"],
-    "Badruu": ["Rosy Bitterling", "Orange Bluegill", "Apple Tree Seed", "Meaty Stir Fry"],
-    "Caleri": ["Grilled Oyster", "Heat Root", "Pinecone", "Bouillabaisse"],
-    "Chayne": ["Onion Seed", "Creamy Carrot Soup", "Dragon's Beard Peat", "Green Pearl"],
-    "Delaila": ["Calico Koi", "Wheat Seed", "Golden Glory Bee", "Giant Goldfish"],
-    "Einar": ["Eyeless Minnow", "Prism Trout", "Vampire Crab", "Stormray"],
-    "Elouisa": ["Calico Koi", "Albino Eel", "Spitfire Cicada", "Ancient Amber Beetle"],
-    "Eshe": ["Grilled Oyster", "Silk", "Macaron", "Jewelwing Dragonfly"],
-    "Hassian": ["Spotted Chapaa Tail", "Chapaa Asada Tacos", "Iron Bar", "Fireworks Arrow"],
-    "Hekla": ["Grilled Mushroom", "Largemouth Bass", "Ramen", "Green Pearl"],
-    "Hodari": ["Fur", "Heartwood", "Iron Bar", "Firebreathing Dragonfly"],
-    "Jel": ["Shell", "Lunar Fairy Moth", "Petit Fives", "Midnight Paddlefish"],
-    "Jina": ["Congee", "Duskwing Butterfly", "Shimmerfin", "Cream of Mushroom Soup"],
-    "Kenli": ["Grilled Fish", "Garden Ladybug", "Princess Ladybug", "Palian Onion Soup"],
-    "Kenyatta": ["Wild Garlic", "Creamy Carrot Soup", "Banded Muujin Mane", "Crimson Fangtooth"],
-    "Nai'O": ["Muujin Meat", "Channel Catfish", "Blueberry Bush Seed", "Steak Dinner"],
-    "Najuma": ["Wild Garlic", "Copper Bar", "Cream of Tomato Soup", "Blueberry Pie"],
-    "Reth": ["Potato", "Ramen", "Raspberry Beetle", "Dari Cloves"],
-    "Sifuu": ["Chapaa Meat", "Standard Arrow", "Iron Bar", "Fireworks Arrow"],
-    "Subira": ["Congee", "Trout Dinner", "Ancient Amber Beetle", "Chapaa Onigiri"],
-    "Tamala": ["Emerald Carpet Moss", "Lunar Fairy Moth", "Hairy Millipede", "Leafstalker Mantis"],
-    "Tau": ["Fried Catfish Dinner", "Unopened Oyster", "Brighteye Butterfly", "Apple"],
-    "Tish": ["Ship Fragments", "Acorn", "Apple Pie", "Green Pearl"],
-    "Zeki": ["Grilled Fish", "Silk", "Pearl", "Gold Ore"]
-
-
-
-
+    "Ashura": ["Silver Salmon", "Striped Sturgeon", "Fish Stew", "Apple Pie"],
+    "Auni": ["Ship Fragments", "Duskwing Butterfly", "Blueberry", "Apple Pie"],
+    "Badruu": ["Potato", "Rice Seed", "Loaded Potato Soup", "Cream of Mushroom Soup"],
+    "Caleri": ["Grilled Mushroom", "Heat Root", "Oysters Akwinduu", "Bouillabaisse"],
+    "Chayne": ["Knapweed", "Rice Seed", "Heat Root", "Green Pearl"],
+    "Delaila": ["Bahari Crab", "WeedBlock Fertilizer", "Apple", "Giant Goldfish"],
+    "Einar": ["Gillyfin", "Ribbontail Ray", "Glow Worm", "Void Ray"],
+    "Elouisa": ["Ship Fragments", "Paddlefish", "Energised Piranha", "Bluebristle Muujin Mane"],
+    "Eshe": ["Fur", "Fish Stew", "Macaron", "Gold Ore"],
+    "Hassian": ["Congee", "Standard Arrow", "Iron Bar", "Fireworks Arrow"],
+    "Hekla": ["Sundrop Lily", "Fried Catfish Dinner", "Chub", "Enchanted Pupfish"],
+    "Hodari": ["Grilled Meat", "Sernuk Noodle Stew", "Loaded Potato Soup", "Firebreathing Dragonfly"],
+    "Jel": ["Barracuda", "Muujin Mane", "Oysters Akwinduu", "Green Pearl"],
+    "Jina": ["Congee", "Cotton Seed", "Dragon's Beard Peat", "Apple Pie"],
+    "Kenli": ["Spotted Chapaa Tail", "Fish Stew", "Sashimi", "Muujin Bahari"],
+    "Kenyatta": ["Briar Daisy", "Elder Sernuk Antlers", "Pinecone", "Fairy Mantis"],
+    "Nai'O": ["Muujin Meat", "Heat Root", "Apple Tree Seed", "Steak Dinner"],
+    "Najuma": ["Clay", "Copper Bar", "Iron Bar", "Silver Bar"],
+    "Reth": ["Yellow Perch", "Heat Root", "Loaded Potato Soup", "Dari Cloves"],
+    "Sifuu": ["Chapaa Meat", "Oily Anchovy", "Iron Bar", "Dispel Arrow"],
+    "Subira": ["Iron Bar", "Fish Stew", "Chili Oil Dumplings", "Firebreathing Dragonfly"],
+    "Tamala": ["Kilima Night Moth", "Garden Millipede", "Hairy Millipede", "Void Ray"],
+    "Tau": ["Calico Koi", "Heartwood Plank", "Golden Salmon", "Wagon Wheel"],
+    "Tish": ["Sapwood Plank", "Acorn", "Apple Pie", "Green Pearl"],
+    "Zeki": ["Silvery Minnow", "Golden Salmon", "Platinum Chad", "Poke Bowl"]
 
 
 };
