c5b439f1fbf733160e3600dad61f63b9b9f731e4
Astrea Luke-Robinson <websitesbyastrea@gmail.com>
2025-06-17 19:33:04 +0930

    Weekly gifts updated

--- jsonFromArray.js
+++ jsonFromArray.js
@@ -10,163 +10,31 @@ let errorString = "";
 let data = {
 
     
-    
-    
- "Ashura": [
-  "Congee",
-  "Heartwood Plank",
-  "Fish Stew",
-  "Celebration Cake"
-],
-"Auni": [
-  "Common Field Cricket",
-  "Proudhorned Stag Beetle",
-  "Apple",
-  "Apple Pie"
-],
-"Badruu": [
-  "Cotton Seed",
-  "Orange Bluegill",
-  "Loaded Potato Soup",
-  "Meaty Stir Fry"
-],
-"Caleri": [
-  "Knapweed",
-  "Silk",
-  "Blueberry Jam",
-  "Inyrevn Mermaid"
-],
-"Chayne": [
-  "Onion",
-  "Creamy Carrot Soup",
-  "Elder Clam Mushroom Meat",
-  "Dari Cloves"
-],
-"Delaila": [
-  "Channel Catfish",
-  "QualityUp Fertilizer",
-  "Mini Mullet",
-  "Rocker Pumpkin"
-],
-"Einar": [
-  "Silver Salmon",
-  "Garden Snail",
-  "Palian Betta",
-  "Fisherman's Brew"
-],
-"Elouisa": [
-  "Common Bark Cicada",
-  "Cytthroat Trout",
-  "Loaded Flowtato",
-  "Rainbow-Tipped Butterfly"
-],
-"Eshe": [
-  "Leather",
-  "Bluefin Tuna",
-  "Silk Thread",
-  "Black Pearl"
-],
-"Hassian": [
-  "Spotted Chapaa Tail",
-  "Fabric",
-  "Sabertoothed Anchovy",
-  "Steak Dinner"
-],
-"Hekla": [
-  "Unopened Oyster",
-  "Painted Perch",
-  "Ramen",
-  "Enchanted Pupfish"
-],
-"Hodari": [
-  "Spotted Chapaa Tail",
-  "Striped Chapaa Tail",
-  "Whitestone Slab",
-  "Spitfire Cicada"
-],
-"Jel": [
-  "Shell",
-  "Silk",
-  "Vampire Crab",
-  "Bean Burger"
-],
-"Jina": [
-  "Mountain Morel",
-  "Brightshroom",
-  "Bacon-Stuffed Mushrooms",
-  "Void Ray"
-],
-"Kenli": [
-  "Spotted Chapaa Tail",
-  "Creamy Carrot Soup",
-  "Sushi",
-  "Gold Bar"
-],
-"Kenyatta": [
-  "Blueberry Jam",
-  "Cytthroat Trout",
-  "Alligator Gar",
-  "Majiri-of-War"
-],
-"Nai'O": [
-  "Muujin Meat",
-  "Pickled Potatoes",
-  "Fried Catfish Dinner",
-  "Veggie Chili"
-],
-"Najuma": [
-  "Flint",
-  "Garden Snail",
-  "Flowtato Fries with Ketchup",
-  "Veggie Chili"
-],
-"Reth": [
-  "Carrot",
-  "Heat Root",
-  "Ogopuu Meat",
-  "Fruit Smoothie Bowl"
-],
-"Sifuu": [
-  "Chapaa Meat",
-  "Oily Anchovy",
-  "Proudhorned Sernuk Antlers",
-  "Gold Bar"
-],
-"Subira": [
-  "Grilled Mushroom",
-  "Fish Stew",
-  "Elderwood Orchid",
-  "Energised Piranha"
-],
-"Tamala": [
-  "Spice Sprouts",
-  "Lunar Fairy Moth",
-  "Hairy Millipede",
-  "Fairy Mantis"
-],
-"Tau": [
-  "Common Blue Butterfly",
-  "Steak Dinner",
-  "Chapaa Masala",
-  "Rainbow-Tipped Butterfly"
-],
-"Tish": [
-  "Crystal Lake Lotus",
-  "Heartwood Plank",
-  "Whitestone",
-  "Blueberry Pie"
-],
-"Zeki": [
-  "Grilled Fish",
-  "Golden Salmon",
-  "Crab Gumbo",
-  "Gold Ore"
-]
-      
-      
-      
-      
-      
+ "Ashura": ["Silver Salmon", "Kilima Catfish", "Honey", "Celebration Cake"],
+  "Auni": ["Waterlogged Boot", "Duskwing Butterfly", "Spotted Mantis", "Apple Pie"],
+  "Badruu": ["Onion Seed", "Hearty Vegetable Soup", "Pumpkin Pie", "Meaty Stir Fry"],
+  "Caleri": ["Sundrop Lily", "Glass Bulb", "Cream of Tomato Soup", "Bouillabaisse"],
+  "Chayne": ["Wild Garlic", "Glass Pane", "Cream of Tomato Soup", "Palian Onion Soup"],
+  "Delaila": ["Rainbow Trout", "QualityUp Fertilizer", "Vampire Crab", "Giant Goldfish"],
+  "Einar": ["Worm", "Mirror Carp", "Fathead Minnow", "Kilima Greyling"],
+  "Elouisa": ["Unopened Oyster", "Paddlefish", "Flowtato", "Giant Goldfish"],
+  "Eshe": ["Fur", "Silk", "Silk Thread", "Steak Dinner"],
+  "Hassian": ["Flint", "Sernuk Hide", "Staircase Mushroom", "Steak Dinner"],
+  "Hekla": ["Oyster Meat", "Brightshroom", "Pinecone", "Green Pearl"],
+  "Hodari": ["Fur", "Heartwood", "Iron Ore", "Steak Dinner"],
+  "Jel": ["Kilima Night Moth", "Duskray", "Vampire Crab", "Midnight Paddlefish"],
+  "Jina": ["Coral", "Duskwing Butterfly", "Bacon-Stuffed Mushrooms", "Dari Cloves"],
+  "Kenli": ["Grilled Fish", "Ramen", "Loaded Flowtato", "Celebration Cake"],
+  "Kenyatta": ["Knapweed", "Creamy Carrot Soup", "Banded Muujin Mane", "Meaty Stir Fry"],
+  "Nai'O": ["Spice Sprouts", "Kilima Catfish", "Bahari Pike", "Fruit Smoothie Bowl"],
+  "Najuma": ["Clay", "Garden Snail", "Stripeshell Snail", "Veggie Chili"],
+  "Reth": ["Potato", "Wheat", "Flowtato Fries with Ketchup", "Fruit Smoothie Bowl"],
+  "Sifuu": ["Sernuk Antlers", "Copper Bar", "Proudhorned Sernuk Antlers", "Silver Bar"],
+  "Subira": ["Grilled Meat", "Fisherman's Brew", "Rainbow-Tipped Butterfly", "Angelfish"],
+  "Tamala": ["Emerald Carpet Moss", "Garden Millipede", "Hairy Millipede", "Fairy Mantis"],
+  "Tau": ["Common Field Cricket", "Silver Salmon", "Azure Stonehopper", "Grilled Meat"],
+  "Tish": ["Coral", "Heartwood Plank", "Flow-Infused Plank", "Blueberry Pie"],
+  "Zeki": ["Unopened Oyster", "Silk", "Fish Tacos", "Poke Bowl"] 
 
 };
 let outputStr = "{\n";
